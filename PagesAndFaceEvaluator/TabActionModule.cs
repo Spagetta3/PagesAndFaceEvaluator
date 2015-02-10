6d171aab5187bc661dc15ccd738ec39ea804f794
Veronika Štrbáková <veronikastrbakova@gmail.com>
2015-02-10 15:06:06 +0100

    bootstrap to saving data

--- PagesAndFaceEvaluator/TabActionModule.cs
+++ PagesAndFaceEvaluator/TabActionModule.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using Nancy;
 using Nancy.Hosting.Self;
@@ -22,11 +23,49 @@ namespace PagesAndFaceEvaluator
 
             Post["/processTabData"] = _ =>
             {
-                /*zavolat funkc\iu na parsovanie, ulozenie a vratit ok....*/
                 ReceivedData data = this.Bind<ReceivedData>();
                 if (data.Data != null)
                 {
-                    return HttpStatusCode.OK;
+                    DateTime timeOfRequest = DateTime.Now;
+                    // ToDO tu stopnut cas a zastavit cas merania pritomnosti tvare
+                    string[] wholeString = data.Data.Split('=');
+                    string urlWithAction = null;
+                    for (int i = 1; i <= wholeString.Length - 1; i++ )
+                        urlWithAction = wholeString[i];
+
+                    wholeString = urlWithAction.Split(' ');
+                    string url = wholeString[0];
+                    string action = wholeString[1];
+                    //string[] filePaths = Directory.GetFiles(@"D:\Programming\Visual Studio 2013\Projects\PagesAndFaceEvaluator\PagesAndFaceEvaluator\Statistics");
+                    string currentPath = url + ".txt";
+                    if (currentPath.StartsWith("http://"))
+                        currentPath = currentPath.Replace("http://","");
+                    else if (currentPath.StartsWith("https://"))
+                        currentPath = currentPath.Replace("https://", "");
+                    currentPath = currentPath.Replace("/", ".");
+                    currentPath = currentPath.Replace("..", ".");
+                    currentPath = currentPath.Replace(":", "");
+ 
+                    string path = @"D:\Programming\Visual Studio 2013\Projects\PagesAndFaceEvaluator\PagesAndFaceEvaluator\statistics\" + currentPath;
+
+                    string rowToWrite = "url: " + url + " action: " + action + " dateOfRequest:" + timeOfRequest;
+                    using (StreamWriter w = File.AppendText(path))
+                    {
+                        w.WriteLine(rowToWrite);
+                        w.Flush();
+                    }
+
+                    string lastPath = ConfigHelper.GetValue(ConfigHelper.ConfigKey.LastPath.ToString());
+                    // ToDo Tu ulozit cas a hodnoty namerane pre poslednu url
+                    if (lastPath != "")
+                    {
+                        lastPath = "";
+                    }
+
+                    if (ConfigHelper.ChangeValue(ConfigHelper.ConfigKey.LastPath.ToString(), path))
+                        return HttpStatusCode.OK;
+                    else
+                        return HttpStatusCode.InternalServerError;
                 }
                 else
                     return HttpStatusCode.InternalServerError;
